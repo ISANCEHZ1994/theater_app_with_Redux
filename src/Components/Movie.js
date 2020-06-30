93f287b75e2b168618f99938d389bf703d2776bb
Israel <Israel.pro1994@gmail.com>
2020-06-30 17:58:53 -0500

    I have poster and movie title

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 
-export default class Movie extends React.Component{
-    render(){
-        return(
-            <>
-            <div style={{color: 'yellow',textAlign: 'center'}}>
-                 an actual moive
-            </div>
+// export default class Movie extends React.Component{
+//     render(){
+//         return(
+//             <>
+//             <div style={{color: 'yellow',textAlign: 'center'}}>
+//             <img src={result.Poster} alt="movie poster" />
+//             <h3> {result.Title}</h3>
+//             </div>
                
-            </>
-        )
-    }
+//             </>
+//         )
+//     }
+// }
+
+export default function Movie({ movie }){
+    return(
+        <div>
+                 <h3> {movie.Title}</h3>
+                <img src={movie.Poster} alt="movie poster" />
+               
+        </div>
+    )
 }
